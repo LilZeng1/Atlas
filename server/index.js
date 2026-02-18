e64ae246dea1f7b08d2fbf043b9a55a0639a74bf
George Arshed <erdemerdem11221154@gmail.com>
2026-02-19 01:00:48 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,31 @@
+// Imports
 import express from "express"
+import bodyParser from "body-parser"
+import fetch from "node-fetch"
+import { Client, Intents } from "discord.js"
 import dotenv from "dotenv"
-import { Client, GatewayIntentBits } from "discord.js"
-
 dotenv.config()
 
+// App()
 const app = express()
-app.use(express.json())
-app.use(express.static("public"))
+app.use(bodyParser.json())
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] })
+// .Env
+const BOT_TOKEN = process.env.BOT_TOKEN
+const GUILD_ID = process.env.GUILD_ID
 
-client.login(process.env.BOT_TOKEN)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] })
+
 
 app.post("/api/assign-role", async (req, res) => {
-    const { roleId } = req.body
-    const guild = await client.guilds.fetch(process.env.GUILD_ID)
-    const member = await guild.members.fetch(process.env.TEST_USER_ID)
-    await member.roles.add(roleId)
-    res.json({ success: true })
+    const roleId = req.body.roleId
+    const userId = req.body.userId
+    if (!roleId || !userId) return res.status(400).send({ error: "Missing parameters" })
+    const url = `https://discord.com/api/v10/guilds/${GUILD_ID}/members/${userId}/roles/${roleId}`
+    const result = await fetch(url, { method: "PUT", headers: { Authorization: `Bot ${BOT_TOKEN}` } })
+    if (result.ok) res.send({ success: true })
+    else res.status(result.status).send({ error: "Failed" })
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
+client.login(BOT_TOKEN)
